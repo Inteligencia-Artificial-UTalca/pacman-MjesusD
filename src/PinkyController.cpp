69628e6a72ee1e6e6996a6dba9d032b43cea83a3
MjesusD <mduarte22@alumnos.utalca.cl>
2026-05-11 23:18:04 -0400

    BT Pinky - Laboratorio 8
    Implementación de estados y adaptación de BTController en PinkyController.

--- src/PinkyController.cpp
+++ src/PinkyController.cpp
@@ -1,17 +1,70 @@
 #include "PinkyController.h"
+#include "BTGhostController.h"
+#include "BehaviorTree.h"
+#include "Ghost.h"
 
+#include <iostream>
+#include <chrono>
 
-PinkyController::PinkyController(std::shared_ptr<Character> character):
-	Controller(character){
+PinkyController::PinkyController(
+	std::shared_ptr<Character> character
+):
+	Controller(character),
+	root(std::make_shared<Selector>())
+{
+
+	//////////////////// FrightnedState ////////////////////
+
+	auto frightenedFilter =
+		std::make_shared<Filter>();
+
+	frightenedFilter->addCondition(
+		std::make_shared<Powerpill>()
+	);
+
+	frightenedFilter->addAction(
+		std::make_shared<Frightened>()
+	);
+
+	root->addChild(frightenedFilter);
+
+	//////////////////// ScatterState ///////////////////////
+
+	auto scatterFilter =
+		std::make_shared<Filter>();
+
+	scatterFilter->addCondition(
+		std::make_shared<TimeOut>()
+	);
+
+	scatterFilter->addAction(
+		std::make_shared<Scatter>()
+	);
+
+	root->addChild(scatterFilter);
+
+	//////////////////// ChaseState /////////////////////////
+
+	root->addChild(
+		std::make_shared<Chase>()
+	);
 }
 
-PinkyController::~PinkyController() {
+PinkyController::~PinkyController(){
 
 }
 
-Move
-PinkyController::getMove(const GameState& game){
-	return PASS;	
+Move PinkyController::getMove(
+	const GameState& game
+){
 
-	
+	Info::getInfo()->in_character =
+		character;
+
+	Info::getInfo()->in_gamestate =
+		&game;
+
+	root->tick();
+
+	return Info::getInfo()->out_move;
 }
--- src/PinkyController.h
+++ src/PinkyController.h
@@ -1,13 +1,24 @@
 #pragma once
 
 #include "Controller.h"
+#include "BehaviorTree.h"
+#include "BTGhostController.h"
 
+class PinkyController : public Controller {
 
-class PinkyController: public Controller {
+private:
+
+	std::shared_ptr<Selector> root;
 
 public:
-	PinkyController(std::shared_ptr<Character> character);
-	virtual ~PinkyController();
-	virtual Move getMove(const GameState& game)override;
-};
 
+	PinkyController(
+		std::shared_ptr<Character> character
+	);
+
+	virtual ~PinkyController();
+
+	virtual Move getMove(
+		const GameState& game
+	) override;
+};
